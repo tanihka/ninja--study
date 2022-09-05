0ec4b89484cf6f7e89146831e38b39a70a75c561
Tanishka <taanishkag078@gmail.com>
2022-09-06 00:36:18 +0530

    .

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,32 +1,29 @@
 import React from 'react'
 import "./slider.css"
 import "../../components/Navbar/navbar.css"
+import public1 from '../../videos/public1.mp4'
+import public3 from '../../videos/public3.mp4'
+import public4 from '../../videos/public4.mp4'
 
 
 const Slider = () => {
     return (
         <>
-            <div className="sliderWrap">
+            <div className="round">
             <div className="faq-heading">The New Way to Learn
                 English Speaking 😃</div>
-            <div className="slider">
-                <div className="slide">
-                    <i class="fa-solid fa-arrow-left-long"></i>
-                    <img src={require('../../images/app1.png')} alt="" />
-                    <i class="fa-solid fa-arrow-right-long"></i>
+            <div className='glide'>
+                <div className='glides'>
+                    
+                    <video src={public1} autoPlay loop muted/>
                 </div>
-                <div className="sliderContent">
-                    <div className='point'>
-                        <i class="fa-solid fa-hand-point-right"></i>
-                        <h4>BUILD CONFIDENCE</h4>
-                    </div>
-                    <p>Gain confidence by practicing speaking in a safe, nonjudgmental environment. </p>
-
-                    <div className='point'>
-                        <i class="fa-solid fa-hand-point-right"></i>
-                        <h4> PRACTICE ENGLISH</h4>
-                    </div>
-                    <p>You can practice English communication on any topic you wish.</p>
+                <div className='glides'>
+                  
+                    <video src={public3} autoPlay loop muted/>
+                </div>
+                <div className='glides'>
+                   
+                    <video src={public4} autoPlay loop muted/>
                 </div>
             </div>
             </div>
