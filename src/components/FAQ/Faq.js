d8af1e26caa12ae29af38f1979d0c421067d0a5f
tanishka gupta <95035224+tanihka@users.noreply.github.com>
2022-09-28 16:33:39 +0530

    Update Faq.js

--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -12,7 +12,7 @@ const Faq = () => {
 
     return (
         <>
-            <div className="faq-heading" id="faq">Answers to FAQs ;)</div>
+            <div className="faq-heading" id="faq">Frequently Asked Questions</div>
             <div className="faq">
             <div className="qna">
                     <QNA/>
