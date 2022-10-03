983be63a0a0de994b77fddd1c65c4583cc09b667
tanishka gupta <95035224+tanihka@users.noreply.github.com>
2022-10-03 13:34:25 +0530

    Update Form.js

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -8,23 +8,26 @@ const Form = () => {
     // className="contact-inputs"
     >
         <div className="row">
-        <div className="name">
-        <label>* Enter your email:</label>
-        <input type="email" name="user_email" placeholder='your@email.com'/>
-        </div>
-        <div className="name">
-        <label>* Enter your name:</label>
-        <input type="text" name="user_name" placeholder='Your name'/>
-        </div>
-        </div>
         
         <div className="name">
+        <label>* Enter your name:</label>
+        <input type="text" name="user_name" placeholder='Tanishka Gupta'/>
+        </div>
+        <div className="name">
+        <label>* Enter your email:</label>
+        <input type="email" name="user_email" placeholder='tanishka@email.com'/>
+        </div>
+        </div>
+        <div className="contact-name-btn">
+        <div className="name">
         <label>* Enter your message:</label>
-        <textarea name="message"placeholder='Enter text here' />
+        <textarea name="message"placeholder='NinjaStudy is a personal AI English tutor App, focused 
+improving your speaking skills. You can think of it as ' />
         </div>
         <div className="btn">
         {/* <input type="submit" value="Send" />  */}
-        <button className='submitbtn'>Send <i class="fa-solid fa-arrow-right-long"></i></button>
+        <button className='submitbtn'>Submit</button>
+        </div>
         </div>
   </form>
   </>
