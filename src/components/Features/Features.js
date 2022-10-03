d132e4113e46a314c63e0e528cbd99a3327044af
tanishka gupta <95035224+tanihka@users.noreply.github.com>
2022-10-03 14:13:48 +0530

    Update Features.js

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -7,7 +7,6 @@ const Features = () => {
     <>
 
       <div className="featuresWrap">
-//         <h4>Explore Feature</h4>
        <div className="features">
         <div className="feature1 reverse">
             <div className="image">
