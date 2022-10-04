39af700cabc4a8954eac027d226913cfad7f3660
Tanishka <taanishkag078@gmail.com>
2022-10-04 18:27:05 +0530

    Updated

---
+++ src/components/Testimonial/Testimonial.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Swiper from './Swiper'
+import './testimonial.css'
+
+const Testimonial = () => {
+  return (
+    <div className="Testimonial" id='features'>
+      <div className="testimonials">
+
+        <h2>What Our Students Say About Us</h2>
+        <Swiper />
+      </div>
+
+    </div>
+  )
+}
+
+export default Testimonial
